337b0f42f8202c11c58bdef93bc98e3bb0a7aafe
DSATISH327 <sathishdesaboina42@gmail.com>
2025-04-09 02:48:59 +0530

    one last commit today

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
             <h2>Project-1</h2>
             <h4>Desaboina Satish Portfolio link <FontAwesomeIcon icon={faLink} /></h4>
             <FontAwesomeIcon icon={faArrowDown} className='arrow-Down'/>
-            <a href="https://photography-alpha-three.vercel.app/"  target="_blank" rel="noopener noreferrer" className='project-link'>Desaboina Satish Portfolio</a>
+            <a href="https://satish-portfolio-indol.vercel.app/"  target="_blank" rel="noopener noreferrer" className='project-link'>Desaboina Satish Portfolio</a>
         </div>
         <div className='project2'>
             <h2>Project-2</h2>
@@ -26,7 +26,7 @@ const Projects = () => {
             <h2>Project-3</h2>
             <h4>Laptops E-Comerce Platform <FontAwesomeIcon icon={faLink} /></h4>
             <FontAwesomeIcon icon={faArrowDown} className='arrow-Down'/>
-            <a href="https://photography-alpha-three.vercel.app/"  target="_blank" rel="noopener noreferrer" className='project-link'>Laptops E-Comerce PLatform</a>
+            <a href="https://desaboina-satish-react-js-project.vercel.app/"  target="_blank" rel="noopener noreferrer" className='project-link'>Laptops E-Comerce PLatform</a>
         </div>
       </div>
     </>
