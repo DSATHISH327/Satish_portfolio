f9dfa315001335abbb9751faf7650a27ef4ea629
DSATISH327 <sathishdesaboina42@gmail.com>
2025-04-09 02:52:56 +0530

    one last commit today at 2:52PM

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
       <nav className='Navbar' id='Home'>
         <h1 className='Headline'>Desaboina Satish</h1>
         <ul>
-            <li><a href="#Contactme">{List_items[0]}</a></li>
-            <li><a href="#Home">{List_items[1]}</a></li>
-            <li><a href="#Projects">{List_items[2]}</a></li>
+            <li><a href='#Home' >{List_items[0]}</a></li>
+            <li><a href="#Projects">{List_items[1]}</a></li>
+            <li><a href="#Contactme">{List_items[2]}</a></li>
             <li><a href="https://drive.google.com/file/d/1iPwsOaUkLdypI6fZlmJ0lpQFg-Hz6ngt/view?usp=drive_link" target="_blank" rel="noopener noreferrer">{List_items[3]}</a></li>
         </ul>
       </nav>
