b5a3cf1f98a7f3ab369d97330eaf17395b60fb67
DSATISH327 <sathishdesaboina42@gmail.com>
2025-04-09 02:46:29 +0530

    last commit at stage 4

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
             <li><a href="#Contactme">{List_items[0]}</a></li>
             <li><a href="#Home">{List_items[1]}</a></li>
             <li><a href="#Projects">{List_items[2]}</a></li>
-            <li><a href="public\Desaboina_satish_resume_web_developer.pdf" target="_blank" rel="noopener noreferrer">{List_items[3]}</a></li>
+            <li><a href="https://drive.google.com/file/d/1iPwsOaUkLdypI6fZlmJ0lpQFg-Hz6ngt/view?usp=drive_link" target="_blank" rel="noopener noreferrer">{List_items[3]}</a></li>
         </ul>
       </nav>
     </>
