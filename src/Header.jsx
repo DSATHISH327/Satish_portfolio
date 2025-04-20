6f9b8362caca725c4c7a521a8eef1be4d058cc71
DSATISH327 <sathishdesaboina42@gmail.com>
2025-04-20 18:55:01 +0530

    updated resume

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
             <li><a href='#Home' >{List_items[0]}</a></li>
             <li><a href="#Projects">{List_items[1]}</a></li>
             <li><a href="#Contactme">{List_items[2]}</a></li>
-            <li><a href="https://drive.google.com/file/d/1iPwsOaUkLdypI6fZlmJ0lpQFg-Hz6ngt/view?usp=drive_link" target="_blank" rel="noopener noreferrer">{List_items[3]}</a></li>
+            <li><a href="https://drive.google.com/file/d/1Lk0gebn731n23ktGFCGRkOqyigcCI4BI/view?usp=drive_link" target="_blank" rel="noopener noreferrer">{List_items[3]}</a></li>
         </ul>
       </nav>
     </>
